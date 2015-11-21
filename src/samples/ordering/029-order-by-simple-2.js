4af23129983b18b08832e11b32de9b9900b66cd7
Tania Shatilova <tatashat@gmail.com>
2015-11-22 00:20:58 +0300

    + Ordering Operators

--- src/samples/ordering/029-order-by-simple-2.js
+++ src/samples/ordering/029-order-by-simple-2.js
@@ -1,9 +1,9 @@
 (function() {
     "use strict";
 
-    var words = ["cherry", "apple", "blueberry"]; 
+    let words = ["cherry", "apple", "blueberry"]; 
 
-    var sortedWords = words.slice().sort((w1, w2) => w1.length - w2.length);
+    let sortedWords = words.slice().sort((w1, w2) => w1.length - w2.length);
 
     console.log("The sorted list of words (by length):"); 
     sortedWords.forEach(w => console.log(w));
