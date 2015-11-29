997a723fff394450a9fafa80e385bfd648d9431e
Tania Shatilova <tatashat@gmail.com>
2015-11-29 21:52:50 +0300

    * typeOf operator

--- src/samples/conversionoperations/057-of-type.js
+++ src/samples/conversionoperations/057-of-type.js
@@ -3,9 +3,7 @@
 
     let numbers = [null, 1.0, "two", 3, "four", 5, "six", 7.0];
 
-    let doubles = numbers.filter(n => {
-        return n === Number(n) && n % 1 !== 0;
-    });
+    let doubles = numbers.filter(n => n === Number(n) && n % 1 !== 0);
 
     console.log("Numbers stored as doubles:"); 
     doubles.forEach(d => console.log(d));
